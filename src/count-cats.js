e1df42766565dbdc230d1b34bd253efdc4332b4d
Artamonov Anatoly <79615328922@yandex.ru>
2022-10-15 10:53:43 +0300

    count cts

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function countCats(arr){
+   let count=0
+  for (i=0; i <arr.length; i++){
+    for(k=0; k <arr[i].length; k++){
+     
+     if(arr[i][k]=== '^^') {count+=1}
+      console.log(count)
+      
+    }
+  }
+    return count
+  }
 
 module.exports = {
   countCats
